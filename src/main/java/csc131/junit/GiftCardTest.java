636a5212f0d2b818c1429a155b56cb797960974c
Peeja Khorashadi <peejakhorashadi@peejas-lappy.lan>
2021-03-14 18:19:11 -0700

    completed testing suite

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -4,9 +4,14 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThrows;
+
+import java.time.Month;
+
 public class GiftCardTest
 {
-@Test
+	@Test
     public void getIssuingStore()
     {
         double       balance;
@@ -19,16 +24,18 @@ public class GiftCardTest
                      issuingStore, card.getIssuingStore());
     
     } 
+	@Test
 	public void getBalance()
 	{
 		double       balance;
         GiftCard     card;
         int          issuingStore;
-        issuingStore = 1337;
+        issuingStore = 1335;
         balance      = 100.00;
         card = new GiftCard(issuingStore, balance);
         assertEquals("getBalance()", balance, card.getBalance(), 0.001);
 	}
+	@Test
 	public void deduct_RemainingBalance() {
 		double       balance;
         GiftCard     card;
@@ -39,4 +46,45 @@ public class GiftCardTest
         card = new GiftCard(issuingStore, balance);
         assertEquals("deduct(50.00)", str, card.deduct(50.00));
 	}
+	@Test
+	public void invalidTransaction() {
+		double       balance;
+	    GiftCard     card;
+	    int          issuingStore;
+	    String str = "Invalid Transaction";
+		issuingStore = 1337;
+		balance      = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("Invalid Transaction", str, card.deduct(-50.00));
+	}
+	@Test
+	public void overdraft() {
+		double       balance;
+	    GiftCard     card;
+	    int          issuingStore;
+	    String str = "Amount Due: " + String.format("%6.2f",25.0);
+	    issuingStore = 1337;
+	    balance      = 25.00;
+	    card = new GiftCard(issuingStore, balance);
+	    assertEquals("overdraft", str, card.deduct(50.00));
+	}
+	@Test
+	public void balanceTooLow() {
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	@Test
+	public void constructor_IncorrectID_Low() {
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,-100.00);});
+	}
+	@Test
+	public void constructor_IncorrectID_High() {
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(100000,-100.00);});
+	}
+
+	 @Test
+	 public void valueOfTest()
+	 {
+	 assertSame("valueOf(DECEMBER)",
+	 Month.DECEMBER, Month.valueOf("DECEMBER"));
+	 }
 }
